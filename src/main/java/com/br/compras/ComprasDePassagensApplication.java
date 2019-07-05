f2b2aca13b46085493e1e25087df40c8769b93b9
Bruno <Bruno.cardoso@zup.com.br>
2019-07-05 17:40:37 -0300

    Criando projeto

---
+++ src/main/java/com/br/compras/ComprasDePassagensApplication.java
@@ -0,0 +1,13 @@
+package com.br.compras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprasDePassagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComprasDePassagensApplication.class, args);
+	}
+
+}
